72fa023f04b79bd19560b283effddfb0ebfb88d0
Kal Carvalho <kalcarvalho@gmail.com>
2021-07-03 17:26:44 -0300

    Alterações 03.07.2021 17.26

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController {
+class DefaultController extends AbstractController {
 
     public function __construct() {
 
@@ -16,6 +17,7 @@ class DefaultController {
      */
 
     public function index() {
+        return $this->render('login.html.twig');
         return new Response("Meu Primeiro Controller");
     }
 
